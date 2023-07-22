c72103e1cf089ee15102119409350d6b0d11e910
Daisy Hong <hdaisywd@gmail.com>
2023-07-22 12:54:16 +0900

    navbar 완성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+// import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -19,9 +25,12 @@ function App() {
     >
       {/* footer를 위해 추가 */}
       <header>
-        <Navbar className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="/"> {/* header에 사진 추가 */}
+      {['sm'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="/">
+              {' '}
+              {/* header에 사진 추가 */}
               <img
                 src={require('./logo.svg').default}
                 width="150"
@@ -30,14 +39,50 @@ function App() {
                 alt="push logo"
               />
             </Navbar.Brand>
-
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/products">Products</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
           </Container>
         </Navbar>
+      ))}
       </header>
       <div className="App" style={{ flex: 1 }}>
-        <nav>
-          <Link to="/about">About</Link> | <Link to="/products">Products</Link>
-        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
